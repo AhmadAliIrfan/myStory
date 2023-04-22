25fb9139fe6775ca2f935c46b59fa7afc8dc8437
AhmadAliIrfan <AhmadAliIrfan@users.noreply.github.com>
2023-04-22 20:32:13 +0500

    Update URL

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const db = require("./db");
 const port = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "https://melodic-jalebi-c05672.netlify.app",
+  origin: "https://famous-dolphin-0ed2d4.netlify.app",
   optionsSuccessStatus: 200,
   credentials: true
 };
