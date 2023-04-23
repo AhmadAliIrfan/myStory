dcfbbb9c83d2c715f25d6eb87bf1c61f55635f23
AhmadAliIrfan <AhmadAliIrfan@users.noreply.github.com>
2023-04-23 16:49:51 +0500

    File Updated

--- app.js
+++ app.js
@@ -29,13 +29,9 @@ app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join('https://idyllic-blancmange-ac4915.netlify.app/', 'index.html'));
- });
 
 db.on("error", console.error.bind(console, "MonoDB Connection Error:"));
 db.once("open", () => {
