f85cdd4bc27e8a4e5450b0f7813f81c1fadb0ec1
AhmadAliIrfan <AhmadAliIrfan@users.noreply.github.com>
2023-05-01 19:18:12 +0500

    New

--- app.js
+++ app.js
@@ -10,39 +10,30 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const path = require('path');
-
-
-
 const db = require("./db");
-
 const port = process.env.PORT || 3001;
-
 const corsOptions = {
-
-  origin:'https://exquisite-stroopwafel-0dc817.netlify.app',
+  //https://remarkable-crostata-a4622f.netlify.app
+  //"https://adorable-twilight-a6b863.netlify.app"
+  //http://localhost:3000
+  //https://cheerful-lollipop-79dc13.netlify.app
+  origin:  'https://exquisite-stroopwafel-0dc817.netlify.app',
   optionsSuccessStatus: 200,
   credentials: true
-};
-
+}; 
+  
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(cors({  origin:'https://exquisite-stroopwafel-0dc817.netlify.app',
-  optionsSuccessStatus: 200,
-  credentials: true}));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
-
-
-
 db.on("error", console.error.bind(console, "MonoDB Connection Error:"));
 db.once("open", () => {
   console.log("Database Connected...");
 });
-
 app.use("/", routes);
-
 app.listen(port, () => {
   console.log("The Express Server is running on port 3001");
 });
